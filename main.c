a974b76090a4e43745a6f8ccdb69e89c6ee3fd87
SmokeyZZZ <sciarrinopie@gmail.com>
2025-07-23 16:02:14 +0200

    added the apple generation

--- main.c
+++ main.c
@@ -2,38 +2,77 @@
 #include <raylib.h>
 #include <unistd.h>
 
-#define ROW_SIZE 50
+#define ROW_SIZE 30
 #define WIDTH 600
-#define HEIGHT 400
+#define HEIGHT 600
 
 typedef enum {
     LEFT,
     RIGHT,
     UP,
     DOWN,
+    NONE
 } Direction;
+
+typedef struct{
+    int x;
+    int y;
+}Apple;
 
 typedef struct {
     int x;
     int y;
     Direction d;
+    int apples;
 } Player;
 
 //if there's a change of direction it changes the property of the player, otherwise it continues the movement as it was doing before the check
 // firstly i need to check the direction
 void checkDirection(Player* p){
-    if(IsKeyDown(KEY_D)||IsKeyDown(KEY_RIGHT)){
+    if(IsKeyPressed(KEY_D)||IsKeyPressed(KEY_RIGHT)){
         p->d=RIGHT;
-    }else if(IsKeyDown(KEY_A)||IsKeyDown(KEY_LEFT)){
+    }else if(IsKeyPressed(KEY_A)||IsKeyPressed(KEY_LEFT)){
         p->d=LEFT;
     }
-    else if (IsKeyDown(KEY_S)||IsKeyDown(KEY_DOWN)){
+    else if (IsKeyPressed(KEY_S)||IsKeyPressed(KEY_DOWN)){
         p->d =DOWN;
     }
-    else if(IsKeyDown(KEY_W)|| IsKeyDown(KEY_UP)){
+    else if(IsKeyPressed(KEY_W)|| IsKeyPressed(KEY_UP)){
         p->d = UP;
     }
     
+
+}
+bool isApplePositionAcceptable(Apple* a ,Player* p){
+    //at the moment i only check if the head of the snake and the apple dont collide
+    if(a->x == p->x && a->y == p->y )return false;
+    return true;
+
+}
+void positionApple(Apple* a , Player* p){
+    //before i set the apple position i should check that the new position of the apple isnt the same position of the snake, in every part of the snake
+    do{
+        a->x = GetRandomValue(0,WIDTH/ROW_SIZE);
+        a->y = GetRandomValue(0,HEIGHT/ROW_SIZE);
+    }while(!isApplePositionAcceptable(a,p));
+    
+
+
+}
+void checkCollision(Apple* a,  Player* p){
+    if(a->x == p->x && p->y == a->y){
+        p->apples++;
+        positionApple(a,p);
+    }
+}
+
+void setUpRandomly(Player* p,Apple* a ){
+    p->x = GetRandomValue(0,WIDTH/ROW_SIZE);
+    p->y = GetRandomValue(0,HEIGHT/ROW_SIZE);
+    printf("starting position (%d,%d)",p->x,p->y);
+    p->d = NONE;
+    p->apples=0;
+    positionApple(a,p);
 }
 void movePlayer(Player* p){
     switch(p->d){
@@ -49,6 +88,8 @@ void movePlayer(Player* p){
         case DOWN:
             p->y+=1;
             break;
+        default:
+            printf("no direction");
     }
     if(p->x > WIDTH/ROW_SIZE){
         p->x = 0;
@@ -61,33 +102,38 @@ void movePlayer(Player* p){
     }else if(p->y <0){
         p->y = HEIGHT/ROW_SIZE;
     }
+    
 }
-void printGame(Player p){
-    /*print a grid WIDTH-HEIGHT with squares with a border darker */
+void printGame(Player p,Apple a){
+    /*print a grid WIDTH-HEIGHT with squares */
     for(int y = 0;y<HEIGHT/ROW_SIZE ;y++){
         for(int x = 0; x < WIDTH/ROW_SIZE;x++){
             DrawRectangleLines(x*ROW_SIZE,y*ROW_SIZE ,ROW_SIZE, ROW_SIZE,WHITE); 
             
         }
     }
+    //draw the player
     DrawRectangle(p.x*ROW_SIZE,p.y*ROW_SIZE,ROW_SIZE,ROW_SIZE,GREEN);
+    //draw the apple
+    DrawRectangle(a.x*ROW_SIZE,a.y*ROW_SIZE,ROW_SIZE,ROW_SIZE,RED);
 }
 
 int main(void)
 {
     InitWindow(WIDTH, HEIGHT, "Snake");
-    SetTargetFPS(10);
+    SetTargetFPS(20);
     Player p;
-    p.x = 5;
-    p.y= 5;
+    Apple a;
+    setUpRandomly(&p,&a);
     while (!WindowShouldClose()){
         
         BeginDrawing();
         ClearBackground(BLACK);
-        printGame(p);
+        printGame(p,a);
         checkDirection(&p);
         movePlayer(&p);
-        printf("(%d)",p.d );
+        checkCollision(&a,&p);
+        printf("posizione apple (%d)(%d)",a.x,a.y);
         EndDrawing();
         
     }
