756cbd44d57a276788cc0fde71ff54bcc0ed39ef
SmokeyZZZ <sciarrinopie@gmail.com>
2025-07-27 22:56:38 +0200

    implemented the adding of new blocks to the tail when eating an apple , movement should be fixed cause it seems too weird

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <raylib.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #define ROW_SIZE 30
 #define WIDTH 600
 #define HEIGHT 600
 
+int apples;
 typedef enum {
     LEFT,
     RIGHT,
@@ -23,8 +25,7 @@ typedef struct {
     int x;
     int y;
     Direction d;
-    int apples;
-    
+    struct Player* next;
 } Player;
 
 //if there's a change of direction it changes the property of the player, otherwise it continues the movement as it was doing before the check
@@ -37,13 +38,11 @@ void checkDirection(Player* p){
         p->d=LEFT;
     }
     if (IsKeyPressed(KEY_S)||IsKeyPressed(KEY_DOWN)){
-        p->d =DOWN;
+        p->d=DOWN;
     }
     if(IsKeyPressed(KEY_W)|| IsKeyPressed(KEY_UP)){
-        p->d = UP;
+        p->d=UP;
     }
-    
-
 }
 bool isApplePositionAcceptable(Apple* a ,Player* p){
     //at the moment i only check if the head of the snake and the apple dont collide
@@ -57,18 +56,34 @@ void positionApple(Apple* a , Player* p){
         a->x = GetRandomValue(0,WIDTH/ROW_SIZE-1);
         a->y = GetRandomValue(0,HEIGHT/ROW_SIZE-1);
     }while(!isApplePositionAcceptable(a,p));
-    
-
-
+}
+//Function that implements linked list adding to the end 
+//critica
+Player* addANode(Player* tail){
+    Player* newNode = malloc(sizeof(Player));
+    newNode->x = tail->x;
+    newNode->y = tail->y;
+    newNode->d = tail->d;
+    newNode->next = NULL;
+    tail->next = newNode;
+    return newNode;
 }
 
-
-//TODO: Function that implements linked list to the snake structure in order to let it expand when it eats an apple.
-void checkCollision(Apple* a,  Player* p){
-    if(a->x == p->x && p->y == a->y){
-        p->apples++;
-        positionApple(a,p);
+void checkCollision(Apple* a,  Player* head,Player** tail){
+    if(a->x == head->x && head->y == a->y){
+        apples++;
+        positionApple(a,head);
+        *tail = addANode(*tail);
     }
+}
+void printList(Player* head){
+    Player* temp = head;
+    printf("[");
+    while(temp!=NULL){
+        printf("(x:%d , y:%d)",temp->x,temp->y);
+        temp=(Player*)temp->next;
+    }
+    printf("]");
 }
 
 void setUpRandomly(Player* p,Apple* a ){
@@ -76,10 +91,12 @@ void setUpRandomly(Player* p,Apple* a ){
     p->y = GetRandomValue(0,HEIGHT/ROW_SIZE-1);
     printf("starting position (%d,%d)",p->x,p->y);
     p->d = NONE;
-    p->apples=0;
+    p->next = NULL;
+    apples=0;
     positionApple(a,p);
 }
-void movePlayer(Player* p){
+//critica
+void moveSinglePiece(Player* p){
     switch(p->d){
         case LEFT:
             p->x -=1;
@@ -106,46 +123,65 @@ void movePlayer(Player* p){
         p->y = 0; 
     }else if(p->y <0){
         p->y = (HEIGHT/ROW_SIZE)-1;
+
     }
-    
 }
-void printGame(Player p,Apple a){
+//critica
+void movePlayer(Player* head){
+    Player* temp =  head;
+    Direction previousDirection = temp->d;
+    Direction tmpDir;
+    while(temp!=NULL){
+        moveSinglePiece(temp);
+        tmpDir = temp->d;
+        temp->d =previousDirection;
+        previousDirection =tmpDir;
+
+        temp=(Player*)temp->next;
+    }
+}
+void drawSnake(Player* head){
+    Player* temp =  head;
+    while(temp!=NULL){
+        DrawRectangle((*temp).x*ROW_SIZE,(*temp).y*ROW_SIZE,ROW_SIZE,ROW_SIZE,GREEN);
+        temp=(Player*)temp->next;
+    }
+}
+void printGame(Player* head,Apple a){
     /*print a grid WIDTH-HEIGHT with squares */
     char appleToString[4];
     for(int y = 0;y<HEIGHT/ROW_SIZE ;y++){
         for(int x = 0; x < WIDTH/ROW_SIZE;x++){
-            DrawRectangleLines(x*ROW_SIZE,y*ROW_SIZE ,ROW_SIZE, ROW_SIZE,WHITE); 
-            
+            DrawRectangleLines(x*ROW_SIZE,y*ROW_SIZE ,ROW_SIZE, ROW_SIZE,WHITE);    
         }
     }
     //draw the player
-    DrawRectangle(p.x*ROW_SIZE,p.y*ROW_SIZE,ROW_SIZE,ROW_SIZE,GREEN);
+    drawSnake(head);
     //draw the apple
     DrawRectangle(a.x*ROW_SIZE,a.y*ROW_SIZE,ROW_SIZE,ROW_SIZE,RED);
-    sprintf(appleToString,"%d",p.apples);
+    sprintf(appleToString,"%d",apples);
+    //that's not the final ui 
     DrawText(appleToString, (0.9)*WIDTH,20,20,WHITE);
 }
 
 int main(void)
 {
     InitWindow(WIDTH, HEIGHT, "Snake");
-    SetTargetFPS(20);
-    Player p;
+    SetTargetFPS(12);
+    Player* head = malloc(sizeof(Player));
+    Player* tail = head ;
     Apple a;
-    setUpRandomly(&p,&a);
+    setUpRandomly(head,&a);
     while (!WindowShouldClose()){
-        
         BeginDrawing();
         ClearBackground(BLACK);
-        printGame(p,a);
-        checkDirection(&p);
-        movePlayer(&p);
-        checkCollision(&a,&p);
-        printf("posizione apple (%d)(%d)",a.x,a.y);
+        printGame(head,a);
+        checkDirection(head);
+        movePlayer(head);
+        checkCollision(&a,head,&tail);
+        printList(head);
         EndDrawing();
-        
     }
     CloseWindow();
-
     return 0;
 }
